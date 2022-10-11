192ae1242e4e6d06b61aa61ee19044f9cb76c9f7
steerven <97967195+steerven@users.noreply.github.com>
2022-10-11 16:56:21 -0300

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
 import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, PaisModule, SharedModule],
+  imports: [BrowserModule, FormsModule, PaisModule, SharedModule, AppRoutingModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
