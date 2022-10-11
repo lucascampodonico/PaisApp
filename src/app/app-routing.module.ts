177207c840af8d6ae9eb7f25df4d4c207a3c3b2e
steerven <97967195+steerven@users.noreply.github.com>
2022-10-11 16:53:09 -0300

    1 file added

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PorRegionComponent } from './pais/pages/por-region/por-region.component';
+
+const routes: Routes = [
+  {
+    path: 'region',
+    component: PorRegionComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
