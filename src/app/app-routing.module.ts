ca2562e35ad0ca34c58f0e7954aeccec1899c284
steerven <97967195+steerven@users.noreply.github.com>
2022-10-12 10:38:45 -0300

    3 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PorPaisComponent } from './pais/pages/por-pais/por-pais.component';
 import { PorRegionComponent } from './pais/pages/por-region/por-region.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: PorPaisComponent,
+    pathMatch: 'full'
+  },
   {
     path: 'region',
     component: PorRegionComponent,
