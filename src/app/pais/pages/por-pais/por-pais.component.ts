798ad85db2cf90082f37beda8b272c46c3eb0b52
steerven <97967195+steerven@users.noreply.github.com>
2022-10-12 14:21:04 -0300

    2 files modified

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PaisService } from '../../services/pais.service';
 import { Country } from '../interfaces/pais.interface';
 
