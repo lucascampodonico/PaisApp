7df867772917edce84ba333c0011e2347b265427
steerven <97967195+steerven@users.noreply.github.com>
2022-10-12 10:55:59 -0300

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, PaisModule, SharedModule, AppRoutingModule],
+  imports: [BrowserModule, FormsModule, PaisModule, SharedModule, AppRoutingModule, HttpClientModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,8 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class PaisService {
 
-  constructor() { }
+  private apiUrl: string = 'https://restcountries.com/v2';
 
+  constructor(private _http: HttpClient) { }
+
+  buscarPais(termino: string): Observable<any>{
+    const url = `${this.apiUrl}/name/${termino}`;
+
+    return this._http.get(url);
+  }
 }
