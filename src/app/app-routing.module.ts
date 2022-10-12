b7a1d5bdc01a45c436594666535817dde04a31bd
steerven <97967195+steerven@users.noreply.github.com>
2022-10-12 09:05:30 -0300

    4 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PorRegionComponent } from './pais/pages/por-region/por-region.component';
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   declarations: [SidebarComponent],
   exports: [SidebarComponent]
 })
