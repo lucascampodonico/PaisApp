d96cbe12318adfd2413440ae9d5798536780c861
steerven <97967195+steerven@users.noreply.github.com>
2022-10-12 10:28:05 -0300

    2 files modified

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SidebarComponent } from './sidebar/sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
+
+import { SidebarComponent } from './sidebar/sidebar/sidebar.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule],
   declarations: [SidebarComponent],
-  exports: [SidebarComponent]
+  exports: [SidebarComponent],
 })
 export class SharedModule {}
